ea648d7f5e494ba6d0158247eea73fa25b0cf2f4
Quang Minh Pham <minhpham10b@gmail.com>
2021-03-24 19:10:43 -0700

    is this report service?

--- src/main/java/com/yojana/services/project/ReportService.java
+++ src/main/java/com/yojana/services/project/ReportService.java
@@ -1,5 +1,25 @@
 package com.yojana.services.project;
 
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yojana.access.ProjectManager;
+import com.yojana.model.project.Report;
+import com.yojana.security.annotations.Secured;
+
+@Path("/reports")
+@Secured
 public class ReportService {
+	
+	@Inject
+	private ProjectManager projectManager;
+	
+	public String toJson(Report report) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(report);
+		return json;
+	}
 
 }
